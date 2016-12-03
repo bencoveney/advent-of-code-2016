136cb28fa5238df351957a8a83b2e2d99e0251b0
Ben Coveney <bencoveney@gmail.com>
2016-12-03 18:27:25 +0000

    Day 1 Part 2

--- 1.js
+++ 1.js
@@ -1,3 +1,4 @@
+
 var data = "R4, R4, L1, R3, L5, R2, R5, R1, L4, R3, L5, R2, L3, L4, L3, R1, R5, R1, L3, L1, R3, L1, R2, R2, L2, R5, L3, L4, R4, R4, R2, L4, L1, R5, L1, L4, R4, L1, R1, L2, R5, L2, L3, R2, R1, L194, R2, L4, R49, R1, R3, L5, L4, L1, R4, R2, R1, L5, R3, L5, L4, R4, R4, L2, L3, R78, L5, R4, R191, R4, R3, R1, L2, R1, R3, L1, R3, R4, R2, L2, R1, R4, L5, R2, L2, L4, L2, R1, R2, L3, R5, R2, L3, L3, R3, L1, L1, R5, L4, L4, L2, R5, R1, R4, L3, L5, L4, R5, L4, R5, R4, L3, L2, L5, R4, R3, L3, R1, L5, R5, R1, L3, R2, L5, R5, L3, R1, R4, L5, R4, R2, R3, L4, L5, R3, R4, L5, L5, R4, L4, L4, R1, R5, R3, L1, L4, L3, L4, R1, L5, L1, R2, R2, R4, R4, L5, R4, R1, L1, L1, L3, L5, L2, R4, L3, L5, L4, L1, R3";
 
 data = data.split(", ");
@@ -17,6 +18,13 @@ var state = {
   direction: "n"
 };
 
+var locations = [];
+var duplicate;
+
+function log(location, name) {
+  console.log((name ? name + " was n: " : "n: ") + location.n + ", e: " + location.e + ", total: " + location.total);
+}
+
 data.forEach(function(turn) {
   var nextDirection;
   switch(state.direction)
@@ -34,10 +42,28 @@ data.forEach(function(turn) {
       nextDirection = turn.direction === "R" ? "n" : "s";
       break;
   }
-  state[nextDirection] += turn.distance;
   state.direction = nextDirection;
+  
+  for(i = 0; i < turn.distance; i++)
+  {
+    state[nextDirection] += 1;
+    
+    var nextLocation = {n: state.n - state.s, e: state.e - state.w};
+    nextLocation.total = Math.abs(nextLocation.n) + Math.abs(nextLocation.e);
+  
+    if (!duplicate)
+    {
+      locations.forEach(function(location) {
+        if(location.n === nextLocation.n && location.e === nextLocation.e)
+        {
+          duplicate = nextLocation;
+        }
+      });
+    }
+  
+    locations.push(nextLocation);
+  }
 });
 
-var total = Math.abs(state.n - state.s) + Math.abs(state.e - state.w)
-
-console.log(total);
+log(locations[locations.length - 1], "final");
+log(duplicate, "duplicate");
